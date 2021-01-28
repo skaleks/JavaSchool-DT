3f601135eb6d09e4882bf765fe7bcf350927e42d
Skalek <skaleksandrovich@inbox.ru>
2021-01-28 14:28:04 +0300

    Add logic for options

---
+++ src/main/java/com/magenta/crud/contract/dto/EditContractDto.java
@@ -0,0 +1,13 @@
+package com.magenta.crud.contract.dto;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class EditContractDto {
+
+    private int optionId;
+    private int contractId;
+}
---
+++ src/main/java/com/magenta/crud/option/dto/EditTariffOptionList.java
@@ -0,0 +1,12 @@
+package com.magenta.crud.option.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class EditTariffOptionList {
+
+    private int tariffId;
+    private int optionId;
+}
